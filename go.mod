840170f08809907063175067c3fd2911ade2771f
Josh Chu <>
2019-04-07 18:59:32 +0800

    Add strategy lsma

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/joshchu00/finance-go-chooser
 
 require (
 	github.com/golang/protobuf v1.3.1
-	github.com/joshchu00/finance-go-common v0.1.1-0.20190407024656-3b55ebbca6ce
+	github.com/joshchu00/finance-go-common v0.1.1-0.20190407104042-d8f0759ed0ef
 	github.com/joshchu00/finance-protobuf v0.1.1-0.20190110032506-7ccfb2f0b013
+	gopkg.in/inf.v0 v0.9.1
 )
--- twse/twse.go
+++ twse/twse.go
@@ -3,6 +3,7 @@ package twse
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"time"
 
 	"github.com/golang/protobuf/proto"
@@ -10,6 +11,7 @@ import (
 	"github.com/joshchu00/finance-go-common/datetime"
 	"github.com/joshchu00/finance-go-common/kafka"
 	"github.com/joshchu00/finance-go-common/logger"
+	"github.com/joshchu00/finance-go-common/strategy"
 	protobuf "github.com/joshchu00/finance-protobuf/inside"
 )
 
@@ -27,22 +29,91 @@ func Process(symbol string, period string, ts int64, client *cassandra.Client, p
 
 	logger.Info(fmt.Sprintf("%s: %s", "Starting twse process...", datetime.GetTimeString(ts, location)))
 
-	message := &protobuf.Notifier{
-		Exchange: "TWSE",
-		Symbol:   symbol,
-		Period:   period,
-		Datetime: ts,
-		Strategy: "lsma",
-	}
+	var irs []*cassandra.IndicatorRow
 
-	var bytes []byte
-
-	bytes, err = proto.Marshal(message)
+	irs, err = client.SelectIndicatorRowsByPartitionKey(
+		&cassandra.IndicatorPartitionKey{
+			Exchange: "TWSE",
+			Symbol:   symbol,
+			Period:   period,
+		},
+	)
 	if err != nil {
 		return
 	}
 
-	producer.Produce(topic, 0, bytes)
+	in := make([]*strategy.LSMAInput, 0)
+
+	for _, ir := range irs {
+
+		var sma0060, sma0120, sma0240 float64
+		sma0060, err = strconv.ParseFloat(ir.SMA0060.String(), 64)
+		if err != nil {
+			return
+		}
+		sma0120, err = strconv.ParseFloat(ir.SMA0120.String(), 64)
+		if err != nil {
+			return
+		}
+		sma0240, err = strconv.ParseFloat(ir.SMA0240.String(), 64)
+		if err != nil {
+			return
+		}
+
+		in = append(
+			in,
+			&strategy.LSMAInput{
+				SMA0060: sma0060,
+				SMA0120: sma0120,
+				SMA0240: sma0240,
+			},
+		)
+	}
+
+	stg := strategy.LSMA
+
+	values := strategy.CalculateLSMA(in)
+
+	for i, ir := range irs {
+
+		if datetime.GetTimestamp(ir.Datetime) >= ts {
+
+			value := values[i]
+
+			client.InsertStrategyRowStringColumn(
+				&cassandra.StrategyPrimaryKey{
+					StrategyPartitionKey: cassandra.StrategyPartitionKey{
+						Exchange: "TWSE",
+						Symbol:   symbol,
+						Period:   period,
+					},
+					Datetime: ir.Datetime,
+				},
+				stg.Column,
+				string(value),
+			)
+
+			if value != strategy.LSMANIL {
+
+				message := &protobuf.Notifier{
+					Exchange: "TWSE",
+					Symbol:   symbol,
+					Period:   period,
+					Datetime: ts,
+					Strategy: "lsma",
+				}
+
+				var bytes []byte
+
+				bytes, err = proto.Marshal(message)
+				if err != nil {
+					return
+				}
+
+				producer.Produce(topic, 0, bytes)
+			}
+		}
+	}
 
 	return
 }
